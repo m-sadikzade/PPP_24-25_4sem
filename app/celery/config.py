732a2c6ed083bc2cb34570689d2b1845d507aba2
Mirakhmad <m.sadikzade@mail.ru>
2025-05-29 14:47:07 +0300

    Delete app directory

--- app/celery/config.py
+++
@@ -1,9 +0,0 @@
-# app/celery/config.py
-from celery import Celery
-
-celery_app = Celery(
-    "tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
-    include=["app.celery.tasks"]
-)
