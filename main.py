459c079ac0543edeb78a7148bb21bd057b84a2f3
Mirakhmad <m.sadikzade@mail.ru>
2025-05-29 14:46:43 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from fastapi import FastAPI
-from app.api import tasks, websocket
-
-app = FastAPI(title="FastAPI Binarization Service", version="0.1.0")
-
-app.include_router(tasks.router)
-app.include_router(websocket.ws_router)
